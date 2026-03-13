51bbb3abf6e1318c519ea53d9148e8ae1a9d41cd
Mohd Sadique <mohdsadique757@gmail.com>
2026-03-13 11:06:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Java Maven Jenkins Sample Github !");
+    }
+
+    public static String greet() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testGreeting() {
+        assertEquals("Hello World", App.greet());
+    }
+}
